4fe285b7b384f5de634b01a5593e5ecd6b0cff99
HaiznLegend <128734209+HaiznLegend@users.noreply.github.com>
2023-03-24 12:13:32 -0400

    New button functionality

--- crochet.js
+++ crochet.js
@@ -1,5 +1,7 @@
 let countEl = document.getElementById("count-el")
 let count = 0
+let loveEl = document.getElementById("love-el")
+let love = "I love you infinity X "
 function control(e){
     if(e.keyCode == 32){
         increment()
@@ -8,4 +10,18 @@ function control(e){
 function increment() {
     count += 1
     countEl.textContent = count
+}
+function subtract() {
+    if (count >= 1) {
+        count -= 1
+        countEl.textContent = count
+    }
+}
+function reset() {
+    count = 0
+    countEl.textContent = count
+}
+function generate() {
+    love = "I love you infinity X " + Math.floor(Math.random()*99) + 1
+    loveEl.textContent = love
 }
